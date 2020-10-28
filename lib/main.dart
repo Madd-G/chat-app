f02585bab8f0fca5a9007381039860d832c9e621
akhmad nur alamsyah <akhmadnuralamsyah@gmail.com>
2020-10-29 00:00:42 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:chat/screens/login_screen.dart';
+import 'package:chat/screens/registration_screen.dart';
+import 'package:chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+//        theme: ThemeData.light().copyWith(
+//          scaffoldBackgroundColor: Colors.transparent,
+//          textTheme: TextTheme(
+//            bodyText2: TextStyle(color: Colors.black),
+//        ),
+      initialRoute: LoginScreen.id,
+      routes: {
+//        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
